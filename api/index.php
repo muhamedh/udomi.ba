5611e7283bf83eb1ef88de5946f91dbb1713969d
muhamedh <muhammed.hamzic@gmail.com>
2022-03-24 14:06:29 +0100

    Added routes for pets

--- api/index.php
+++ api/index.php
@@ -4,15 +4,54 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once DIRNAME(__FILE__).'dao/';
+require_once  dirname(__FILE__)."../../vendor/autoload.php";
+require_once  dirname(__FILE__)."/dao/BaseDao.class.php";
+require_once  dirname(__FILE__)."/dao/PetsDao.class.php";
 
 Flight::register('basedao', 'BaseDao');
 
 Flight::register('petsdao', 'PetsDao');
+
+
 /**
 * List all pets
 */
 Flight::route('GET /pets', function(){
   Flight::json(Flight::petsdao()->get_all_pets());
 });
+
+/**
+* List pet by id
+*/
+Flight::route('GET /pets/@pet_id', function($pet_id){
+  Flight::json(Flight::petsdao()->get_pet_by_id($pet_id));
+});
+
+/**
+* List pet by vaccine
+*/
+
+Flight::route('GET /pets/@vaccinated', function($vaccinated){
+  Flight::json(Flight::petsdao()->get_pets_by_vaccine($vaccinated));
+});
+
+/**
+* List pet by age
+*/
+
+Flight::route('GET /pets/@timestamp', function($timestamp){
+  Flight::json(Flight::petsdao()->get_pets_by_age($timestamp));
+});
+
+/**
+* Add pet
+*/
+Flight::route('POST /pets', function(){
+  Flight::json(Flight::petsdao()->insert_pet(Flight::request()->data->getData()));
+});
+
+
+
+
+Flight::start();
 ?>
