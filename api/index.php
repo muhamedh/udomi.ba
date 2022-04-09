e197950ccff2acb4f3c11eb5299c821b9ea750e3
muhamedh <muhammed.hamzic@gmail.com>
2022-04-09 15:56:37 +0200

    index.php reorg + test all routes

--- api/index.php
+++ api/index.php
@@ -15,9 +15,11 @@ Flight::register('petsdao', 'PetsDao');
 Flight::register('speciesdao', 'SpeciesDao');
 Flight::register('usersdao', 'UsersDao');
 
+/// ALL PETS TABLE ROUTES
 
 /**
 * List all pets
+* W
 */
 Flight::route('GET /pets', function(){
   Flight::json(Flight::petsdao()->get_all_pets());
@@ -25,6 +27,7 @@ Flight::route('GET /pets', function(){
 
 /**
 * List pet by id
+* W
 */
 Flight::route('GET /pets/@pet_id', function($pet_id){
   Flight::json(Flight::petsdao()->get_pet_by_id($pet_id));
@@ -32,6 +35,7 @@ Flight::route('GET /pets/@pet_id', function($pet_id){
 
 /**
 * List pet by vaccine
+* W
 */
 
 Flight::route('GET /pets/vac/@vaccinated', function($vaccinated){
@@ -40,6 +44,7 @@ Flight::route('GET /pets/vac/@vaccinated', function($vaccinated){
 
 /*
 * Get female pets
+* W
 */
 Flight::route('GET /pets/gender/@gender', function($gender){
   Flight::json(Flight::petsdao()->get_by_gender($gender));
@@ -47,25 +52,32 @@ Flight::route('GET /pets/gender/@gender', function($gender){
 
 /**
 * List all pets which are older than provided date
+* W
 */
-
 Flight::route('GET /pets/older/@timestamp', function($timestamp){
   Flight::json(Flight::petsdao()->get_older_pets($timestamp));
 });
+
 /*
 * List all pets which are younger than provided date
+* W
 */
 Flight::route('GET /pets/younger/@timestamp', function($timestamp){
   Flight::json(Flight::petsdao()->get_younger_pets($timestamp));
 });
 
 /**
-* Add pet
+* Add pet to database
+* W
 */
 Flight::route('POST /pets', function(){
   Flight::json(Flight::petsdao()->insert_pet(Flight::request()->data->getData()));
 });
 
+/**
+* Delete pet by its id
+* W
+*/
 Flight::route('DELETE /pets/@id', function($id){
   Flight::petsdao()->deletePet($id);
   Flight::json(["message" => "deleted"]);
@@ -74,14 +86,15 @@ Flight::route('DELETE /pets/@id', function($id){
 /** SPECIES ROUTES **/
 /*
 * Get all species
+* W
 */
-
 Flight::route('GET /species', function(){
   Flight::json(Flight::speciesdao()->get_all_species());
 });
 
 /*
 * Get species by species_id
+* W
 */
 
 Flight::route('GET /species/@id', function($id){
@@ -90,6 +103,7 @@ Flight::route('GET /species/@id', function($id){
 
 /*
 * Get species by pets_id
+* W
 */
 
 Flight::route('GET /species/pets/@pets_id', function($pets_id){
@@ -97,26 +111,43 @@ Flight::route('GET /species/pets/@pets_id', function($pets_id){
 });
 
 
-//User routes
-
+/** USERS ROUTES **/
+/*
+* Get all users
+* W
+*/
 
 Flight::route('GET /users', function(){
   Flight::json(Flight::usersdao()->getAllUsers());
 });
-
+/*
+* Get user by its id
+* W
+*/
 Flight::route('GET /users/@user_id', function($user_id){
   Flight::json(Flight::usersdao()->getUserById($user_id));
 });
 
-//ne radi
+/*
+* Get user by username
+* L -> assigned Rania
+*/
+
 Flight::route('GET /users/@username', function($username){
   Flight::json(Flight::usersdao()->getUsername($username));
 });
-
+/*
+* Insert a new user into the database
+* W
+*/
 Flight::route('POST /users', function(){
   Flight::json(Flight::usersdao()->insertUser(Flight::request()->data->getData()));
 });
 
+/*
+* Update user info
+*
+*/
 
 Flight::route('PUT /users/@id', function($id){
   $request = Flight::request();
@@ -125,7 +156,10 @@ Flight::route('PUT /users/@id', function($id){
   Flight::json(['message' => 'updated']);
 });
 
-//works
+/*
+* Delete a user by its id from the database
+* W
+*/
 Flight::route('DELETE /users/@id', function($id){
   Flight::usersdao()->deleteUser($id);
   Flight::json(["message" => "deleted"]);
