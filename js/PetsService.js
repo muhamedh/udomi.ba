f34356c3d4c718899ddba712ddb0c3b9590a628c
rania-w <rania.weiss@stu.ibu.edu.ba>
2022-04-13 10:48:19 +0200

    smol fix

--- js/PetsService.js
+++ js/PetsService.js
@@ -40,24 +40,24 @@ var PetsService = {
     $.get("api/pets/" + id, function (data) {
       $("#pets-list").attr('hidden', true);
       $("#individual-pet").html("");
+      console.log(data[0].pet_gender);
       var html = "";
-      for (let i = 0; i < data.length; i++) {
         html += `
         <section>
           <div class="container px-4 px-lg-5 my-5">
             <div class="row gx-4 gx-lg-5 align-items-center">
-              <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="` + data[i].photos_url + `" alt="..."></div>
+              <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="` + data.photos_url + `" alt="..."></div>
               <div class="col-md-6">
-                <h1 class="display-5 fw-bolder">` + data[i].petname + `</h1>
+                <h1 class="display-5 fw-bolder">` + data.petname + `</h1>
                 <div class="fs-5 mb-2">
-                  <span>` + data[i].pet_gender + `</span>
-                  <span>` + data[i].pet_birthdate + `</span>
-                  <span>` + data[i].vaccinated + `</span>
+                  <span>` + data.pet_gender + `</span>
+                  <span>` + data.pet_birthdate + `</span>
+                  <span>` + data.vaccinated + `</span>
                 </div>
                 <div class="fs-5 mb-2">
                   <span>ako budemo još info dodavali</span>
                 </div>
-                <p class="lead">` + data[i].pets_description + `</p>
+                <p class="lead">` + data.pets_description + `</p>
                 <div class="d-flex">
                   <button class="btn btn-outline-success flex-shrink-0" type="button">Kontaktirajte vlasnika</button>
                 </div>
@@ -66,11 +66,11 @@ var PetsService = {
           </div>
         </section>
         `
-      }
+      
       document.getElementById("loading-spinner").style.display = "none";
       $("#individual-pet").html(html);
       console.log(data);
-    })
+    });
   },
 
   get: function (id) {
