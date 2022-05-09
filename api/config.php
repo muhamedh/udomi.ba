f43197bd2ff8eee305b77212dd933ebe74ebbed9
muhamedh <muhammed.hamzic@gmail.com>
2022-05-09 22:11:57 +0200

    config file changes

--- api/config.php
+++ api/config.php
@@ -3,16 +3,16 @@
 class Config{
 
   public static function DB_HOST(){
-    return Config::get_env("DB_HOST", "sql11.freemysqlhosting.net");
+    return Config::get_env("DB_HOST", "localhost");
   }
   public static function DB_USERNAME(){
-    return Config::get_env("DB_USERNAME", "sql11479683");
+    return Config::get_env("DB_USERNAME", "root");
   }
   public static function DB_PASSWORD(){
-    return Config::get_env("DB_PASSWORD", "SYjx6V3AZc");
+    return Config::get_env("DB_PASSWORD", "root123");
   }
   public static function DB_SCHEME(){
-    return Config::get_env("DB_SCHEME", "sql11479683");
+    return Config::get_env("DB_SCHEME", "udomidb");
   }
   public static function DB_PORT(){
     return Config::get_env("DB_PORT", "3306");
@@ -21,11 +21,6 @@ class Config{
   public static function get_env($name, $default){
     return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
    }
-
-  /*const DB_HOST = "sql11.freemysqlhosting.net";
-  const DB_USERNAME = "sql11479683";
-  const DB_PASSWORD = "SYjx6V3AZc";
-  const DB_SCHEME = "sql11479683";*/
 
 }
 
