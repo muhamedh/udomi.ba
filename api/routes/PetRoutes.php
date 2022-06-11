8248fbfd7cf2b020bdb3a66b2b28f2c4d5a4d7cd
muhamedh <muhammed.hamzic@gmail.com>
2022-06-11 15:43:25 +0200

    hotfix 2

--- api/routes/PetRoutes.php
+++ api/routes/PetRoutes.php
@@ -121,7 +121,7 @@ Flight::route('POST /private/pets', function(){
 
   if($user['user_id'] == $data['owner_id']){
     //Flight::json
-    (Flight::petsService()->pet_pets_photos_add($data));
+    Flight::json(Flight::petsService()->pet_pets_photos_add($data));
   }else{
     throw new Exception("This is hack you will be traced, be prepared :)");
   }
